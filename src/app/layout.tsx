b47d5924c90ea36e6628edeb5a92209d59369966
Luis <985812182@qq.com>
2026-05-13 14:28:35 +0800

    Add AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+      <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6232467855116032"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         {children}
         <Analytics />
       </body>
